b79ceef7d7282b82f7dcbb6f367aac4527c53b04
sourabh <sharmasourabh6789@gmal.com>
2023-11-07 10:28:32 -0800

    new changes

--- app/core/models.py
+++ app/core/models.py
@@ -1,2 +1,3 @@
 from django.db import models
-from django.contrib.auth.models import ( AbstractBaseUser, BaseUserManager, PermissionMixin)
+from django.contrib.auth.models import ( AbstractBaseUser, BaseUserManager, PermissionsMixin)
+
